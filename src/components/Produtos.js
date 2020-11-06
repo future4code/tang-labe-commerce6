cedf919132638ea9c4974ca63c9357eebcd91525
helenarangel <helenarangel001@gmail.com>
2020-11-06 20:18:09 -0300

    não deu certo, mas tentei kkk

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isElementOfType } from 'react-dom/test-utils';
 import styled from 'styled-components'
 import Outfit1 from '../imgs/Outfit1.png'
 import Outfit2 from '../imgs/Outfit2.png'   
@@ -9,11 +10,51 @@ import Outfit6 from "../imgs/Outfit6.png"
 
 export default class Produtos extends React.Component {
     state = {
+        produtos: [
+            {
+             id: Date.now(),
+             name: "Traje Espacial Despojado Porém Chique",
+             preco: 50,
+             img: Outfit1
+            },
+            {
+             id: Date.now(),
+             name:"Traje Espacial Esporte Chique",
+             preco: 100,
+             img: Outfit2
+            },
+            {
+             id: Date.now(),
+             name:"Traje Espacial Samus Aram Afrontosa",
+             preco: 150,
+             img: Outfit4
+            },
+            {
+             id: Date.now(),
+             name:"Traje Espacial Bafafá Certeiro",
+             preco: 200,
+             img: Outfit5
+            },
+            {
+             id: Date.now(),
+             name:"Traje Espacial Samus Aram Afrontosa",
+             preco: 250,
+             img: Outfit6
+            },
+            {
+             id: Date.now(),
+             name:"Traje Espacial Samus Aram Afrontosa",
+             preco: 300,
+             img: Outfit6
+            },
+        ],
+        produtosCarrinho: "",
         valorDoProdutoMin: "",
         valorDoProdutoMax: "",
         valorDoProdutoBusca: ""
     }
 
+    
     onchangeValorDoProdutoMin = (event) => {
         this.setState({valorDoProdutoMin: event.target.value})
     }
@@ -26,22 +67,82 @@ export default class Produtos extends React.Component {
         this.setState({valorDoProdutoBusca: event.target.value})
     }
 
+    adicionarAoCarrinho = () => {
+        const produtoCarrinho = {
+            name: this.state.produtos.name,
+            preco: this.state.produtos.preco
+        }
+        const novosProdutos = [...this.state.produtos, produtoCarrinho]
+        this.setState({produtos: novosProdutos})
+        
+    }
+
+    /* adicionarAoCarrinho = (produtoEspecifico) => {
+    const copiaDosProdutos = this.state.produtos.filter((elemento) => {
+        if(elemento.id !== Date.now()){
+            return false;
+        } else {
+            return <p>{elemento.name} - {elemento.preco}</p>
+        }
+    });
+       const produtosNoCarrinho = [...this.state.produtos, copiaDosProdutos]
+       this.setState({produtos: produtosNoCarrinho}) 
+    }       
+     */
     render(){
-        const valordoproduto = [50, 100, 150, 200, 250, 300]
-        /* caso de teste
-        const produtos = () => {
-            for (let i = 0; i < valordoproduto.length; i++) {
-                if(valordoproduto[i] >= this.state.valorDoProdutoMin){
+         const produtoNovo = this.state.produtos.map((novaListaDeProdutos) => {
+           /*  if(novaListaDeProdutos.id === Date.now) */
+            return(
+              <div>
+                <p>{novaListaDeProdutos.name}</p>
+                <p>{novaListaDeProdutos.preco}</p>
+              </div>
+            )
+          }
+          ) 
+        const Filtro = this.state.produtos.map((produto) =>{
+            if(this.state.valorDoProdutoMin !== ""){
+                if(this.state.valorDoProdutoMin <= produto.preco){
                     return <div className="Produto">
-                        <img src={Outfit1}/>
-                        <p>Traje Espacial Despojado Porém Chique</p>
-                        <p>R$ {valordoproduto[0]}</p>
-                        <button id="botaoAdicionar">Comprar</button>
+                        <img src={produto.img}/>
+                        <p>{produto.name}</p>
+                        <p>R$ {produto.preco}</p>
+                        <button id="botaoAdicionar" onClick={this.adicionarAoCarrinho}>Comprar</button>
                     </div>
                 }
             }
-        }
-        */
+
+            else if(this.state.valorDoProdutoMax !== ""){
+                if(this.state.valorDoProdutoMax >= produto.preco){
+                    return <div className="Produto">
+                        <img src={produto.img}/>
+                        <p>{produto.name}</p>
+                        <p>R$ {produto.preco}</p>
+                        <button id="botaoAdicionar" onClick={this.adicionarAoCarrinho}> Comprar</button>
+                    </div>
+                }
+            }
+
+            else if(this.state.valorDoProdutoBusca !== ""){
+                if(this.state.valorDoProdutoBusca === produto.name){
+                    return <div className="Produto">
+                        <img src={produto.img}/>
+                        <p>{produto.name}</p>
+                        <p>R$ {produto.preco}</p>
+                        <button id="botaoAdicionar" onClick={this.adicionarAoCarrinho}>Comprar</button>
+                    </div>
+                }
+            }
+
+            else{
+                return <div className="Produto">
+                    <img src={produto.img}/>
+                    <p>{produto.name}</p>
+                    <p>R$ {produto.preco}</p>
+                    <button id="botaoAdicionar" onClick={this.adicionarAoCarrinho}>Comprar</button>
+                </div>
+            }
+        })
 
         return (
             <div>
@@ -60,57 +161,17 @@ export default class Produtos extends React.Component {
                     </div>
 
                     <div className="PrimeiraLinha">
-                        
-                        <div className="Produto">
-                            <img src={Outfit1}/>
-                            <p>Traje Espacial Despojado Porém Chique</p>
-                            <p>R$ {valordoproduto[0]}</p>
-                            <button id="botaoAdicionar">Comprar</button>
-                        </div>
-
-                        <div className="Produto">
-                            <img src={Outfit2}/>
-                            <p>Traje Espacial Esporte Chique</p>
-                            <p>R$ {valordoproduto[1]}</p>
-                            <button id="botaoAdicionar">Comprar</button>
-                        </div>
-
-                        <div className="Produto">
-                            <img src={Outfit5}/>
-                            <p>Traje Espacial Bafafá Certeiro</p>
-                            <p>R$ {valordoproduto[2]}</p>
-                            <button id="botaoAdicionar">Comprar</button>
-                        </div>
-
-                        <div className="Produto">
-                            <img src={Outfit4}/>
-                            <p>Traje Espacial Samus Aram Afrontosa</p>
-                            <p>R$ {valordoproduto[3]}</p>
-                            <button id="botaoAdicionar">Comprar</button>
-                        </div>
-
-                        <div className="Produto">
-                            <img src={Outfit6}/>
-                            <p>Traje Espacial Samus Aram Afrontosa</p>
-                            <p>R$ {valordoproduto[4]}</p>
-                            <button id="botaoAdicionar">Comprar</button>
-                        </div>
-
-                        <div className="Produto">
-                            <img src={Outfit6}/>
-                            <p>Traje Espacial Samus Aram Afrontosa</p>
-                            <p>R$ {valordoproduto[5]}</p>
-                            <button id="botaoAdicionar">Comprar</button>
-                        </div>
+                        {Filtro}
                     </div>
 
                     <div className="Filtros">
                         <h3>Carrinho</h3>
-                
+                        <p>{produtoNovo}</p>
                     </div>
 
                 </div>
             </div>
         );
+        }
     }
-}
+
